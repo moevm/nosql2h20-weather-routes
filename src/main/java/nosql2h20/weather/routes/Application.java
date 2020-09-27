10900c7b33aecba4b1898dbf5d15400898c09181
laskovenko1 <laskovenko1@yandex.ru>
2020-09-27 16:37:50 +0300

    add hello world app on Spring boot using neo4j

---
+++ src/main/java/nosql2h20/weather/routes/Application.java
@@ -0,0 +1,12 @@
+package nosql2h20.weather.routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
