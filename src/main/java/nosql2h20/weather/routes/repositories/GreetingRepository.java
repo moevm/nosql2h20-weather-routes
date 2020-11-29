5185100361b69c8ab98e420b8b9626f97833f7e2
laskovenko1 <laskovenko1@yandex.ru>
2020-11-29 17:45:15 +0300

    migrate to Quarkus

--- src/main/java/nosql2h20/weather/routes/repositories/GreetingRepository.java
+++ src/main/java/nosql2h20/weather/routes/repositories/GreetingRepository.java
@@ -4,23 +4,22 @@ import nosql2h20.weather.routes.entities.Greeting;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import static org.neo4j.driver.Values.parameters;
 
-@Repository
+@ApplicationScoped
+@Transactional
 public class GreetingRepository {
 
-    private final Driver neo4jDriver;
-
-    @Autowired
-    public GreetingRepository(Driver neo4jDriver) {
-        this.neo4jDriver = neo4jDriver;
-    }
+    @Inject
+    Driver driver;
 
     public Greeting getGreeting(String name) {
-        try (Session session = neo4jDriver.session()) {
+        try (Session session = driver.session()) {
             String nodeGreeting = session.writeTransaction(transaction -> {
                 Result result = transaction.run("CREATE (a:Greeting) " +
                                 "SET a.name = $name " +
