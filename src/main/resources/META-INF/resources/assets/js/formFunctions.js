4ef27d89a429392ac61be848011a1193a1d146fa
kateapl <cakanbr@gmail.com>
2020-12-20 20:02:16 +0300

    done map functions on index page

--- src/main/resources/META-INF/resources/assets/js/formFunctions.js
+++ src/main/resources/META-INF/resources/assets/js/formFunctions.js
@@ -1,9 +1,84 @@
-$(document).ready(function(){
+var from = null
+var to = null
 
-    $.getJSON('GET_OBJECT.json', function(data) {
-        for(var i=0;i<data.objects.length;i++){
-            $('#objects').append('<tr><th>' + data.objects[i].name + '</th><td>' + data.objects[i].street +
-                '</td><td>' + data.objects[i].house_number + '</td><td>' + data.objects[i].points[0].lon + ' ' +  data.objects[i].points[0].lat + '</td><tr>');
-        }
-    });
-});
+function init() {
+    ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map_canvas", {
+                center: [59.9386, 30.3141],
+                zoom: 10
+            });
+
+            myMap.events.add('click', function (e) {
+                var coords = e.get('coords');
+                fromValue = $('#from').val()
+                toValue = $('#to').val()
+                if (fromValue == "") {
+                    myMap.balloon.open(coords, {
+                        contentHeader:'Откуда',
+                    });
+                    var myFromReverseGeocoder = ymaps.geocode(coords);
+                    myFromReverseGeocoder.then(
+                        function (res) {
+                             $('#from').val(res.geoObjects.get(0).properties.get('name'));
+                        },
+                        function (err) {
+                            alert('Ошибка');
+                        }
+                    );
+                } else {
+                    myMap.balloon.open(coords, {
+                        contentHeader:'Куда',
+                    });
+                    var myToReverseGeocoder = ymaps.geocode(coords);
+                    myToReverseGeocoder.then(
+                        function (res) {
+                             $('#to').val(res.geoObjects.get(0).properties.get('name'));
+                        },
+                        function (err) {
+                            alert('Ошибка');
+                        }
+                    );
+                }
+
+            });
+
+            $('#searchbtn').on('click', function(myMap) {
+                fromValue = $('#from').val()
+                toValue = $('#to').val()
+
+                if (fromValue == "" || toValue == "") {
+                    alert("Пожалуйста, заполните поля Откуда и Куда")
+                    return
+                }
+
+                var fromGeocoder = ymaps.geocode(fromValue);
+                var toGeocoder = ymaps.geocode(toValue);
+
+                fromGeocoder.then(
+                    function (res) {
+                        var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                        from = coordinates
+                    },
+                    function (err) {
+                        alert('Ошибка');
+                    }
+                );
+
+                toGeocoder.then(
+                    function (res) {
+                        var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                        to = coordinates
+                        window.localStorage.setItem("from", from)
+                        window.localStorage.setItem("to", to)
+                //        alert('Координаты откуда :' + from + ', координаты куда :' + to);
+                        window.location.href = '/route';
+                    },
+                    function (err) {
+                        alert('Ошибка');
+                    }
+                );
+            })
+    }
+}
+
