d38530d41b773de5c23cd3102efde43e87fb1443
kateapl <cakanbr@gmail.com>
2020-12-20 14:28:42 +0300

    add yandex map

---
+++ src/main/resources/META-INF/resources/assets/js/formFunctions.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $.getJSON('GET_OBJECT.json', function(data) {
+        for(var i=0;i<data.objects.length;i++){
+            $('#objects').append('<tr><th>' + data.objects[i].name + '</th><td>' + data.objects[i].street +
+                '</td><td>' + data.objects[i].house_number + '</td><td>' + data.objects[i].points[0].lon + ' ' +  data.objects[i].points[0].lat + '</td><tr>');
+        }
+    });
+});
