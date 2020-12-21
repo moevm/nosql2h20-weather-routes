be445fc47c820216f0d2579a9b1aadfeb5860a97
laskovenko1 <laskovenko1@yandex.ru>
2020-12-22 02:37:27 +0300

    fix client export

--- src/main/resources/META-INF/resources/assets/js/dataFunctions.js
+++ src/main/resources/META-INF/resources/assets/js/dataFunctions.js
@@ -1,22 +1,23 @@
 function init() {
-     $('#upload_btn').on('click', function() {
-            $.ajax({
+    $('#upload_btn').on('click', function () {
+        $.ajax({
             method: "POST",
             data: $('#file')[0].files[0],
             url: "http://localhost:8080/api/import/map"
-            })
-            .done(function( json ) {
+        }).done(function (json) {
+        });
+    });
 
-            });
-     });
-
-      $('#export_btn').on('click', function() {
-             $.ajax({
-             method: "GET",
-             url:  "http://localhost:8080/api/export"
-             })
-             .done(function( json ) {
-
-             });
-      });
+    $('#export_btn').on('click', function () {
+        $.ajax({
+            url: "api/export"
+        }).done(function (xml) {
+            var xmlText = new XMLSerializer().serializeToString(xml);
+            var blob = new Blob([xmlText], {type: 'text/plain'});
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "database.graphml";
+            link.click();
+        })
+    });
 }
