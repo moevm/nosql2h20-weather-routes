11ed7e25e5f6a5d1de3f864a6dfd2d2147785abb
alskaa <alisagorodeckaa7786@gmail.com>
2020-12-23 13:04:06 +0300

    update requests for data.html

--- src/main/resources/META-INF/resources/assets/js/dataFunctions.js
+++ src/main/resources/META-INF/resources/assets/js/dataFunctions.js
@@ -1,9 +1,31 @@
+var files;
+
 function init() {
+
+    $('input[type=file]').on('change', function(){
+        files = this.files;
+        console.log(files)
+    });
+
     $('#upload_btn').on('click', function () {
+        event.stopPropagation(); // Остановка происходящего
+        event.preventDefault();
+
+        var data = new FormData();
+        $.each( files, function( key, value ){
+            data.append( key, value );
+            console.log(data)
+        });
+
+
         $.ajax({
-            method: "POST",
-            data: $('#file')[0].files[0],
-            url: "http://localhost:8080/api/import/map"
+            url: "http://localhost:8080/api/import/map",
+            method: 'POST',
+            data: data,
+            cache: false,
+            dataType: 'xml',
+            processData: false, // Не обрабатываем файлы (Don't process the files)
+            contentType: false,
         }).done(function (json) {
         });
     });
