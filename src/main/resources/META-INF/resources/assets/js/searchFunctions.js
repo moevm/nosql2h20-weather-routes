a46af5b51414a4d671de5786012eb2d70b6af5a9
laskovenko1 <laskovenko1@yandex.ru>
2020-12-26 18:05:55 +0300

    fix searchFunctions.js

--- src/main/resources/META-INF/resources/assets/js/searchFunctions.js
+++ src/main/resources/META-INF/resources/assets/js/searchFunctions.js
@@ -4,24 +4,7 @@ function init() {
         method: 'GET',
         url: '/api/object/all',
     })
-        .done(function (obj) {
-            $('#objects').find('tr:gt(0)').remove();
-
-            for (let i = 0; i < obj.length; i++) {
-                let info = '<tr><th>' + obj[i].name + '</th><td>' + obj[i].street + '</td><td>' + obj[i].houseNumber + '</td>'
-                let point = '<td><div class="points"><table><tr><th>Широта</th><th>Долгота</th><th>Кол-во осадков</th></tr><tbody>'
-
-                for (let j = 0; j < obj[i].points.length; j++) {
-                    point += '<td>'
-                    point += obj[i].points[j].latitude + '</td><td>'
-                    point += obj[i].points[j].longitude + '</td><td>'
-                    point += obj[i].points[j].precipitationValue + '</td></tr>'
-                }
-                point += '</tbody></table></div></td></tr>';
-
-                $('#objects').append(info + point);
-            }
-        });
+        .done(parseObjects);
 
     $('#searchname').keyup(function () {
         val = $('#searchname').val()
@@ -29,21 +12,7 @@ function init() {
             method: 'GET',
             url: '/api/object/filter/name/' + val,
         })
-            .done(function (obj) {
-                $('#objects').find('tr:gt(0)').remove();
-
-                for (let i = 0; i < obj.length; i++) {
-                    let info = '<tr><th>' + obj[i].name + '</th><td>' + obj[i].street + '</td><td>' + obj[i].houseNumber + '</td>'
-                    let points = '<td> <div class="points">'
-
-                    for (let j = 0; j < obj[i].points.length; j++) {
-                        points += obj[i].points[j].latitude + ' ' + obj[i].points[j].longitude + ' '
-                    }
-                    points += '</div></td><tr>'
-
-                    $('#objects').append(info + points);
-                }
-            });
+            .done(parseObjects);
     })
 
     $('#searchaddr').keyup(function () {
@@ -52,22 +21,27 @@ function init() {
             method: 'GET',
             url: '/api/object/filter/address/' + val,
         })
-            .done(function (obj) {
-                $('#objects').find('tr:gt(0)').remove();
-
-                for (let i = 0; i < obj.length; i++) {
-                    let info = '<tr><th>' + obj[i].name + '</th><td>' + obj[i].street + '</td><td>' + obj[i].houseNumber + '</td>'
-                    let points = '<td> <div class="points">'
-
-                    for (let j = 0; j < obj[i].points.length; j++) {
-                        points += obj[i].points[j].latitude + ' ' + obj[i].points[j].longitude + ' '
-                    }
-                    points += '</div></td><tr>'
-
-                    $('#objects').append(info + points);
-                }
-            });
+            .done(parseObjects);
     })
+
+    function parseObjects(obj) {
+        $('#objects').find('tr:gt(0)').remove();
+
+        for (let i = 0; i < obj.length; i++) {
+            let info = '<tr><th>' + obj[i].name + '</th><td>' + obj[i].street + '</td><td>' + obj[i].houseNumber + '</td>'
+            let points = '<td><div class="points"><table><tr><th>Широта</th><th>Долгота</th><th>Кол-во осадков</th></tr><tbody>'
+
+            for (let j = 0; j < obj[i].points.length; j++) {
+                points += '<td>'
+                points += obj[i].points[j].latitude + '</td><td>'
+                points += obj[i].points[j].longitude + '</td><td>'
+                points += obj[i].points[j].precipitationValue + '</td></tr>'
+            }
+            points += '</tbody></table></div></td></tr>'
+
+            $('#objects').append(info + points);
+        }
+    }
 
     $('#addbtn').on('click', function () {
         nameValue = $('#addname').val()
